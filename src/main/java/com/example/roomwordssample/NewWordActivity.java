a24422d701ead9f9733b2158b7d213f2455919d8
MUHAMMAD WAFIYUDDIN BIN ROHANIZAM <62368837+wafiyR@users.noreply.github.com>
2021-08-29 22:35:03 +0800

    Add files via upload

---
+++ src/main/java/com/example/roomwordssample/NewWordActivity.java
@@ -0,0 +1,66 @@
+package com.example.roomwordssample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+// create constants
+
+import static com.example.roomwordssample.MainActivity.EXTRA_DATA_UPDATE_WORD;
+import static com.example.roomwordssample.MainActivity.EXTRA_DATA_ID;
+
+
+
+public class NewWordActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "me.mahakagg.roomwordssamplev3.REPLY";
+    public static final String EXTRA_REPLY_ID = "me.mahakagg,roomwordssamplev3.REPLY_ID";
+    private EditText mEditWordView;
+//    private Button button_save;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_word);
+        mEditWordView = findViewById(R.id.edit_word);
+        int id = -1;
+        final Bundle extras = getIntent().getExtras();
+
+        if (extras != null){
+            String word = extras.getString(EXTRA_DATA_UPDATE_WORD, "");
+            if (!word.isEmpty()){
+                mEditWordView.setText(word);
+                mEditWordView.setSelection(word.length());
+                mEditWordView.requestFocus();
+            }
+        }
+
+        final Button button = findViewById(R.id.button_save);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent replyIntent = new Intent();
+                if (TextUtils.isEmpty(mEditWordView.getText())){
+                    setResult(RESULT_CANCELED, replyIntent);
+                }
+                else{
+                    String word = mEditWordView.getText().toString();
+                    replyIntent.putExtra(EXTRA_REPLY, word);
+                    if (extras != null && extras.containsKey(EXTRA_DATA_ID)){
+                        int id = extras.getInt(EXTRA_DATA_ID, -1);
+                        if (id != -1){
+                            replyIntent.putExtra(EXTRA_REPLY_ID, id);
+                        }
+                    }
+                    setResult(RESULT_OK, replyIntent);
+                }
+                finish();
+            }
+        });
+    }
+}
